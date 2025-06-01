3d9763c37feca126bc3ae8f919d76ec48412c033
Son Do <dominhson580@gmail.com>
2025-06-01 11:15:50 +0700

    drawing to screen

--- follow-along/browser.py
+++ follow-along/browser.py
@@ -1,5 +1,7 @@
 import socket
 import ssl
+import sys
+import tkinter
 
 
 class URL:
@@ -7,34 +9,44 @@ class URL:
         self.scheme, url = url.split("://", 1)
         assert self.scheme in ["http", "https"]
 
+        # Separate host and path
         if "/" not in url:
             url = url + "/"
         self.host, url = url.split("/", 1)
         self.path = "/" + url
 
+        # Default port
         if self.scheme == "http":
             self.port = 80
         elif self.scheme == "https":
             self.port = 443
 
+        # Separate host and custom port
         if ":" in self.host:
             self.host, port = self.host.split(":", 1)
             self.port = int(port)
 
     def request(self):
+        """Send HTTP request and return response body"""
+
+        # Establish TCP connection
         s = socket.socket(
             family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
         )
         s.connect((self.host, self.port))
+
+        # Upgrade to TLS for HTTPS
         if self.scheme == "https":
             ctx = ssl.create_default_context()
             s = ctx.wrap_socket(s, server_hostname=self.host)
 
+        # Send HTTP GET request
         request = "GET {} HTTP/1.0\r\n".format(self.path)
         request += "Host: {}\r\n".format(self.host)
         request += "\r\n"
         s.send(request.encode("utf8"))
 
+        # Parse response status line and headers
         response = s.makefile("r", encoding="utf8", newline="\r\n")
         status_line = response.readline()
         version, status, explanation = status_line.split(" ", 2)
@@ -47,15 +59,19 @@ class URL:
             header, value = line.split(":", 1)
             response_headers[header.casefold()] = value.strip()
 
+        # Reject chunked or compressed responses
         assert "transfer-encoding" not in response_headers
         assert "content-encoding" not in response_headers
 
+        # Return response body and close connection
         content = response.read()
         s.close()
         return content
 
 
-def show(body):
+def lex(body):
+    """Skip the tags and return text content of HTML document"""
+    text = ""
     in_tag = False
     for c in body:
         if c == "<":
@@ -63,15 +79,67 @@ def show(body):
         elif c == ">":
             in_tag = False
         elif not in_tag:
-            print(c, end="")
+            text += c
+    return text
 
 
-def load(url):
-    body = url.request()
-    show(body)
+WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+
+
+def layout(text):
+    """Collect characters along with with page coordinates"""
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+
+        cursor_x += HSTEP
+        # Line wrap when reaching right edge
+        if cursor_x >= WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+
+    return display_list
+
+
+SCROLL_STEP = 100
+
+
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
+        self.canvas.pack()
+
+        self.scroll = 0
+        self.window.bind("<Down>", self.scroll_down)
+
+    def load(self, url):
+        """Fetch and display content from the given URL"""
+        body = url.request()
+        text = lex(body)
+        self.display_list = layout(text)
+        self.draw()
+
+    def draw(self):
+        """Draw the visible content on the screen"""
+        self.canvas.delete("all")
+
+        for x, y, c in self.display_list:
+            if y > self.scroll + HEIGHT:
+                continue
+            if y + VSTEP < self.scroll:
+                continue
+            self.canvas.create_text(x, y - self.scroll, text=c)
+
+    def scroll_down(self, e):
+        """Scroll downward and redraw the screen"""
+        self.scroll += SCROLL_STEP
+        self.draw()
 
 
 if __name__ == "__main__":
-    import sys
-
-    load(URL(sys.argv[1]))
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop()
